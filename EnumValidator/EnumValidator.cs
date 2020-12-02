b8e572ee11b52f0969d5745612467012f99f5b50
Marc Chapar <marcchapar@hotmail.com>
2020-12-02 18:47:00 +0100

    EnumValidator added

---
+++ EnumValidator/EnumValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EnumValidator
+{
+    public static class EnumValidator
+    {
+        public static bool Validate<T>(string value, out object result)
+        {
+            bool valid = false;
+            value = NormalizerEnum.NormalizeStr(value);
+
+            if (Enum.TryParse(typeof(T), value, true, out var parsedValue))
+            {
+                valid = Enum.IsDefined(typeof(T), parsedValue);
+            }
+
+            result = parsedValue;
+            return valid;
+        }
+
+        public static bool Validate(Type type, string value, out object result)
+        {
+            bool valid = false;
+
+            value = NormalizerEnum.NormalizeStr(value);
+
+            if (Enum.TryParse(type, value, true, out var parsedValue))
+            {
+                valid = Enum.IsDefined(type, parsedValue);
+            }
+
+            result = parsedValue;
+            return valid;
+        }
+    }
+}
---
+++ EnumValidator/README.md
@@ -0,0 +1,3 @@
+# Enum Validator
+
+Helper class to validate Enum values correctly using both IsDefined and TryParse.
